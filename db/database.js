8ba468d43f9ef74f9a1dda480d110164bb5ad285
MAC <mayority11@gmail.com>
2021-11-03 22:13:06 +0100

    updates

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const mongoose_ = require('mongoose')
 const password = 'socialabuja123@@@'
-// const dbURL = `mongodb+srv://socialabuja:${password}@socialabuja-w9tpx.mongodb.net/socialabuja?retryWrites=true&w=majority`;
-const dbURL = `mongodb://127.0.0.1:27017/socialAbuja`
+const dbURL = `mongodb+srv://socialabuja:${password}@socialabuja-w9tpx.mongodb.net/socialabuja?retryWrites=true&w=majority`
+// const dbURL = `mongodb://127.0.0.1:27017/socialAbuja`
 
 module.exports = mongoose_.connect(dbURL, {
   useNewUrlParser: true,
